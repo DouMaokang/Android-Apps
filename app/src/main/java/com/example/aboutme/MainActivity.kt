ab078363cfba2521799226a6133b52d36f74c092
DouMaokang <alecdou0001@gmail.com>
2021-01-17 22:47:04 +0800

    Color My Views app

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,54 +1,60 @@
 package com.example.aboutme
 
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import androidx.databinding.DataBindingUtil
-import com.example.aboutme.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
-    private val myName: MyName = MyName("Aleks Haecky")
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.myName = myName
-//        findViewById<Button>(R.id.done_button).setOnClickListener {
-//            addNickName(it)
-//        }
-        binding.doneButton.setOnClickListener {
-            addNickName(it)
+        setContentView(R.layout.activity_main)
+        setListeners()
+
+    }
+
+    private fun setListeners() {
+        findViewById<TextView>(R.id.box_one_text)
+        val clickableViews: List<View> =
+            listOf(
+                findViewById(R.id.box_one_text),
+                findViewById(R.id.box_two_text),
+                findViewById(R.id.box_three_text),
+                findViewById(R.id.box_four_text),
+                findViewById(R.id.box_five_text),
+                findViewById(R.id.constraint_layout),
+                findViewById(R.id.red_button),
+                findViewById(R.id.green_button),
+                findViewById(R.id.yellow_button),
+            )
+
+        for (item in clickableViews) {
+            item.setOnClickListener {makeColored(it)}
         }
     }
 
-    private fun addNickName(view: View) {
-//        val editText = findViewById<EditText>(R.id.nickname_edit)
-//        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
-//        nicknameTextView.text = editText.text
-//        editText.visibility = View.GONE
-//        view.visibility = View.GONE
-//        nicknameTextView.visibility = View.VISIBLE
+    private fun makeColored(view: View) {
+        when (view.id) {
 
-        binding.apply {
-            myName?.nickname = binding.nicknameEdit.text.toString()
-            // Invalidate all binding expressions and request a new rebind to refresh UI
-            invalidateAll()
-            nicknameEdit.visibility = View.GONE
-            doneButton.visibility = View.GONE
-            nicknameText.visibility = View.VISIBLE
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            R.id.red_button -> findViewById<TextView>(R.id.box_three_text).setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> findViewById<TextView>(R.id.box_four_text).setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> findViewById<TextView>(R.id.box_five_text).setBackgroundResource(R.color.my_green)
+
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
         }
-
-        // Hide the keyboard.
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
